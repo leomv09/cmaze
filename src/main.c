23048e8e5fb938af575d33340c83ba90ab66d890
Jose Andres Garcia <jags9415@gmail.com>
2014-03-13 20:44:42 -0600

    Se creó la función que rellena una matriz de manera aleatoria.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include "matrix.h"
 
 int main (int argc, char *argv[])
 {
-    printf("Hello World");
+    int m_rows, m_cols;
+    m_rows = 3;
+    m_cols = 3;
+    int mtx[m_rows][m_cols];
+    fill_matrix_random(m_rows, m_cols, mtx);
+    print_matrix(m_rows, m_cols, mtx);
     return 0;
 }
---
+++ src/matrix.h
@@ -0,0 +1,7 @@
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+
+void fill_matrix_random(int rows, int cols, int mtx[rows][cols]);
+void print_matrix(int rows, int cols, int mtx[rows][cols]);
+
+#endif // MATRIX_H_INCLUDED
