5e7ea9b9bac993a134f174f7c7a37be6fbf3aff0
Jose Andres Garcia <jags9415@gmail.com>
2014-03-16 21:03:42 -0600

    Se agregó el código que carga el .xml de la interfaz.

--- src/main.c
+++ src/main.c
@@ -3,13 +3,36 @@
 #include <gtk/gtk.h>
 #include "maze.h"
 
-int main (int argc, char *argv[])
+void on_window_destroy (GtkObject *object, gpointer user_data)
 {
+    gtk_main_quit ();
+}
+
+int main (int argc, char* argv[])
+{
+    /*
     Matrix* m = malloc(sizeof(Matrix));
     init_matrix(m, 15, 25);
     print_matrix(m);
     printf("\n");
     make_maze(m);
     print_matrix_as_maze(m);
+    */
+
+    GtkBuilder* builder;
+    GtkWidget* window;
+
+    gtk_init (&argc, &argv);
+
+    builder = gtk_builder_new ();
+    gtk_builder_add_from_file (builder, "src/gui.glade", NULL);
+    window = GTK_WIDGET (gtk_builder_get_object (builder, "main_window"));
+    gtk_builder_connect_signals (builder, NULL);
+
+    g_object_unref (G_OBJECT (builder));
+
+    gtk_widget_show (window);
+    gtk_main ();
+
     return 0;
 }
